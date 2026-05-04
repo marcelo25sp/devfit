32eaf3402dc426ffc52ed0c7985818596190374f
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2026-05-04 01:29:10 -0300

    feat: Cria projeto Spring Boot e integra ao projeto DevFit

---
+++ src/main/java/com/devfit/DevfitApplication.java
@@ -0,0 +1,13 @@
+package com.devfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevfitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevfitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devfit/DevfitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devfit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevfitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
